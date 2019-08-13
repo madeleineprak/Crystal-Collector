21f7f074cada83206e5b23f21d9fa88e0d53ad1d
Madeleine Prak <madeleineprak@gmail.com>
2019-08-12 21:27:14 -0700

    Added html, styling and updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# unit-4-game
+# Crystal Collector
+This is a crystal collector game. Click [here](https://madeleineprak.github.io/unit-4-game/) to play it.
+
+Tools utilized:
+* HTML
+* CSS
+* Bootstrap (See documentation [here](https://getbootstrap.com/).)
+* jQuery 
+
+# How To Play
+You will be given a random number at the start of the game. There are four crystals. By clicking on a crystal you will add a specific amount of points to your total score. You win the game by matching your total score to random number, you lose the game if your total score goes above the random number. The value of each crystal is hidden from you until you click on it. Each time when the game starts, the game will change the values of each crystal.
+
+# Authors
+[Madeleine Prak](https://github.com/madeleineprak)
+
+# Acknowledgements
+Go UW Coding Bootcamp!
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,7 +6,7 @@ var numLosses = 0;
 var win = false;
 var lose = false;
 
-// Sets up numbers for crystals and target score.
+// Sets up game for the beginning and for whenever the player wins/loses.
 function setGame() {
     playerScore = 0;
     win = false;
@@ -15,55 +15,68 @@ function setGame() {
         crystals[i] = Math.floor(Math.random()*12) + 1;
     }
     targetScore = Math.floor(Math.random()*120) + 19;
+    $("#target-score").html(targetScore);
+    $("#player-score").html(playerScore);
     console.log(crystals);
     console.log(targetScore);
 }
 
+// Checks for a win/lose and alerts the user respectively.
 function checkWinOrLose() {
     if (playerScore === targetScore) {
         numWins++;
         win = true;
         alert("You win!");
+        $("#wins").html("Wins: " + numWins);
     } else if (playerScore > targetScore) {
         numLosses++;
         lose = true;
         alert("You lose!");
+        $("#losses").html("Losses: " + numLosses);
     }
 }
 
+// Sets up the game initially.
 setGame();
 
 // When user clicks on the first crystal.
 $("#crystal-one").on("click", function() {
     playerScore += crystals[0];
     console.log("Your score is " + playerScore + " and the target score is " + targetScore);
+    $("#player-score").html(playerScore);
     checkWinOrLose();
     if (win || lose) {
         setGame();
     }
 });
 
+// When user clicks on the second crystal.
 $("#crystal-two").on("click", function() {
     playerScore += crystals[1];
     console.log("Your score is " + playerScore + " and the target score is " + targetScore);
+    $("#player-score").html(playerScore);
     checkWinOrLose();
     if (win || lose) {
         setGame();
     }
 });
 
+// When user clicks on the third crystal.
 $("#crystal-three").on("click", function() {
     playerScore += crystals[2];
     console.log("Your score is " + playerScore + " and the target score is " + targetScore);
+    $("#player-score").html(playerScore);
     checkWinOrLose();
     if (win || lose) {
         setGame();
     }
 });
 
+// When user clicks on the fourth crystal.
 $("#crystal-four").on("click", function() {
     playerScore += crystals[3];
     console.log("Your score is " + playerScore + " and the target score is " + targetScore);
+    $("#player-score").html(playerScore);
     checkWinOrLose();
     if (win || lose) {
         setGame();
